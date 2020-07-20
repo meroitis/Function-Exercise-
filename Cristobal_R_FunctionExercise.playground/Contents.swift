0c38d91d80d67f70d56702931568521a11b96b72
meroitis <67976213+meroitis@users.noreply.github.com>
2020-07-20 13:46:31 -0500

    Add files via upload

---
+++ Cristobal_R_FunctionExercise.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+// Funtion that adds both Ints (numbers
+func addTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+// allows to input both Ints
+// Print of results 
+print("Result: \(addTwoInts(10, 20))")
+
+
